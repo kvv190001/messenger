8bb96cad6f3fcc70bcfe03cc73ed935cb11cc302
Viet Khue Vu <kvv190001@users.noreply.github.com>
2024-07-26 15:39:32 -0500

    initialized react app and created a home page displays all users

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch('/api/users')
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return(
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="App-intro">
+                    <h2> User List </h2>
+                    {users.map(user =>
+                        <div key={user.id}>
+                            {user.username}
+                        </div>
+                    )}
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default App;
