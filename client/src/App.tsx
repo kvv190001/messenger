253f06e20c55f9e223d73ebf9968ea6b63641d61
prestonwscott <prestonwscott@outlook.com>
2024-07-28 19:47:25 -0500

    convert to vite+react TS

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import logo from './assets/smile.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <p>
+          <img src={logo} className="smiley"/>
+          <div>chats</div>
+        </p>
+      </div>
+    </>
+  )
+}
